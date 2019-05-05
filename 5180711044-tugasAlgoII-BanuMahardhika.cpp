b46b28f499fbe72f9564946abe1b3d49faaf55a3
BanuMahardhika44 <49566925+BanuMahardhika44@users.noreply.github.com>
2019-05-05 14:54:29 +0700

    Add files via upload

--- 5180711044-tugasAlgoII-BanuMahardhika.cpp
+++ 5180711044-tugasAlgoII-BanuMahardhika.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
+
 using namespace::std;
 
 int main(){
     string nomor[5]={"A001","A002","A003","A007","A008"};
     string nama[5]={"BAYU","IRVAN","SUSI","DIANA","RONI"};
 
-    string nama1,cr,mmbr,brg,N;
-    int jum=5,indeks,hrg,jumlah,tot;
-    cout<<"Apakah Anda member atau tidak?[t/y] : ";cin>>mmbr;
+    string namaku,cari,member,barang,N;
+    int indeks,hrg,diskon,jumlah,tot,jum=5;
 
- if(mmbr=="t")
+
+    cout<<"Apakah Anda member atau tidak?[t/y] : ";cin>>member;
+
+
+
+ if(member=="t")
     {cout<<"\nSilahkan masukan nama anda:";cin>>N;
      cout<<"\nBerapa jumlah barang yang anda beli : ";cin>>jumlah;
-        cout<<"\nNama barang yang dibeli : ";cin>>brg;
+
+        cout<<"\nNama barang yang dibeli : ";cin>>barang;
         cout<<"\nHarga barang : ";cin>>hrg;
+
         tot=jumlah*hrg;
+
         cout<<"\nJumlah Harga semuanya:"<<tot;
         return 0;}
-      else  if (mmbr=="y"){
-            cout<<"\nSilahakan masukan no id: ";cin>>cr;
+
+      else  if (member=="y"){
+            cout<<"\nSilahakan masukan no id: ";cin>>cari;
             for (int i=0;i<jum;i++){
-                if(cr==nomor[i]){
-                    nama1=nama[i];
+                if(cari==nomor[i]){
+                    namaku=nama[i];
                 }
             }
             cout<<"\nAnda member"<<endl;
-            cout<<"\nId Anda   : "<<cr<<endl;
-            cout<<"\nNama Anda : "<<nama1<<endl;
+            cout<<"\nId Anda   : "<<cari<<endl;
+            cout<<"\nNama Anda : "<<namaku<<endl;
+
             cout<<"\nBerapa jumlah barang yg anda beli : ";cin>>jumlah;
-            cout<<"\nNama barang : ";cin>>brg;
+
+
+            cout<<"\nNama barang : ";cin>>barang;
             cout<<"\nHarga : ";cin>>hrg;
+
             tot=jumlah*hrg;
+
+
             cout<<"\nSelamat anda mendapatkan diskon 5%";
             tot=tot*5/100;
             cout<<"\nJumalah Harga semuanya:"<<tot;
